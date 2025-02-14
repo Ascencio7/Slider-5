44b19e8e0b79fb7872032fd8a070a08de9276e37
Vladimir Ascencio <vladimirramos405@gmail.com>
2025-02-14 13:56:59 -0600

    correcion en el script e inicio del estilo css

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let prevDom = document.getElementById('prev');
 let carouselDom = document.querySelector('.carousel');
 let SliderDom = carouselDom.querySelector('.carousel .list');
 let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySelector('.item');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
 let timeDom = document.querySelector('.carousel .time');
 
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
@@ -12,34 +12,31 @@ let timeRunning = 3000;
 let timeAutoNext = 7000;
 
 nextDom.onclick = function(){
-    showSlider('next');
+    showSlider('next');    
 }
 
 prevDom.onclick = function(){
-    showSlider('prev');
+    showSlider('prev');    
 }
 
 let runTimeOut;
-
 let runNextAuto = setTimeout(() => {
-    next.click(); 
+    next.click();
 }, timeAutoNext)
 
 function showSlider(type){
-    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let  SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
     let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
-
+    
     if(type === 'next'){
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
-    }
-    else{
+    }else{
         SliderDom.prepend(SliderItemsDom[SliderItemsDom.length - 1]);
         thumbnailBorderDom.prepend(thumbnailItemsDom[thumbnailItemsDom.length - 1]);
         carouselDom.classList.add('prev');
     }
-
     clearTimeout(runTimeOut);
     runTimeOut = setTimeout(() => {
         carouselDom.classList.remove('next');
